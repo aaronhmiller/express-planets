7d336ce9095faa0c455446715007d23ab764830b
Aaron Miller <aaron@konghq.com>
2021-05-18 17:24:47 -0700

    fiddling

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res) => {
-  res.send("<h2>Hi There!</h2>");
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
 });
+ 
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+ 
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+ 
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.patch('/', (req, res) => {
+  return res.send('Received a PATCH HTTP method');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port,() => console.log(`listening on port ${port}`));
